f198c6289d0b1c95a56f10b2a2dee3e50e448079
Elton-Henrique-Oliveira <terezafe.oli@gmail.com>
2024-11-09 10:55:27 -0300

    Iniciado Projeto Spring com algumas dependencias já

---
+++ src/main/java/com/br/elton/teste_pratico_votacao/TestePraticoVotacaoApplication.java
@@ -0,0 +1,13 @@
+package com.br.elton.teste_pratico_votacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestePraticoVotacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestePraticoVotacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/elton/teste_pratico_votacao/TestePraticoVotacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.elton.teste_pratico_votacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestePraticoVotacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
